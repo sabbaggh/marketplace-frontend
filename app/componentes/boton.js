33b7612c64398385aeda1f3dadc35e08485980bf
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2025-01-27 21:08:57 -0600

    commit inicial

---
+++ app/componentes/boton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Boton = () => {
+  return (
+    <TouchableOpacity >
+      <Text>Boton</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default Boton
+
+const styles = StyleSheet.create({})
