ec2f1fbdb34749aa94e06d9166d9bff31cb1cd01
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2025-01-27 22:45:27 -0600

    configuracion nativewind y pantalla inicio

--- app/componentes/boton.js
+++ app/componentes/boton.js
@@ -1,14 +1,13 @@
+import '../../global.css'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const Boton = () => {
+const Boton = ({texto,bg}) => {
   return (
-    <TouchableOpacity >
-      <Text>Boton</Text>
+    <TouchableOpacity className={`p-3 ${bg} rounded`}>
+      <Text className='text-blue-500 text-center text-3xl'>{texto}</Text>
     </TouchableOpacity>
   )
 }
 
 export default Boton
-
-const styles = StyleSheet.create({})
--- app/index.js
+++ app/index.js
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import '../global.css';
+import { Text, View, SafeAreaView } from "react-native";
+import Boton from './componentes/boton'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-indigo-50">
+      <View className='bg-indigo-50 h-screen px-4 py-5 flex flex-col justify-center items-center'>
+        <Text className='text-3xl bg-indigo-500 text-center h-4/6'>Logo</Text>
+        <View className='h-2/6 w-5/6 py-12 flex-col justify-evenly'>
+          <Boton texto={"Inicia Sesión"} bg={"bg-indigo-900"}/>
+          <Boton texto={"Registrate"} bg={"bg-indigo-900"}/>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
